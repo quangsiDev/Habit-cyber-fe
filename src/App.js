00cadd2c14935eba192f8a92ca1bec0e44bd42d1
Quang Si <quangsiDev>
2021-06-23 03:22:12 +0700

    add habit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from "./logo.svg";
 import ListContainerContent from "./components/ListContainerContent/ListContainerContent";
+
 function App() {
-  return (
-    <div className="App">
-      <div className="flex bg-gray-200 w-full min-h-full">
-        <ListContainerContent title={"To Do"}></ListContainerContent>
-      </div>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        div className = "flex bg-gray-200 w-full min-h-full" >
+        <
+        ListContainerContent title = { "To Do" } > < /ListContainerContent> <
+        ListContainerContent title = { "Habit" } > < /ListContainerContent> <
+        /div> <
+        /div>
+    );
 }
 
 export default App;
